9c41be5115050c0e5927d3762b40ebac7d06e9f0
HJunng <guswjd1gh@gmail.com>
2024-07-25 17:44:27 +0900

    comment : 신청서 작성 dummy data -> insert 테스트 이후 javafx로 변경 예정.

--- src/main/java/org/example/onesteponestamp/HelloApplication.java
+++ src/main/java/org/example/onesteponestamp/HelloApplication.java
@@ -2,15 +2,22 @@ package org.example.onesteponestamp;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.onesteponestamp.autoapply.AutoApplyService;
+import org.example.onesteponestamp.common.Country;
 import org.example.onesteponestamp.common.DBConnectionManager;
+import org.example.onesteponestamp.common.VisaType;
 
 
 public class HelloApplication extends Application {
     private static Connection conn;
+    private static AutoApplyService autoApplyService = new AutoApplyService();
     
     @Override
     public void start(Stage stage) throws IOException {
@@ -22,11 +29,24 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+//        launch();
 
         // 예시 코드 (connection)
         conn = DBConnectionManager.getInstance().getConnection();
 
+        /**
+         *  신청서 작성 insert
+         *  todo 1 : insert 할 때 trigger 넣어서, 심사 승인 trigger 연결 시켜야 함 (-> db에서 수행)
+         *  todo 2 : 이후 JavaFX로 로직 연결
+         */
+//        autoApplyService.createAutoApply(
+//            "P0001", Country.GBR, "Emma Watsob", "F",
+//            LocalDate.parse("2020/01/02", DateTimeFormatter.ofPattern("yyyy/MM/dd")),
+//            LocalDate.parse("2029/07/25", DateTimeFormatter.ofPattern("yyyy/MM/dd")),
+//            LocalDate.parse("1980/01/02", DateTimeFormatter.ofPattern("yyyy/MM/dd")),
+//            VisaType.STUDENT_VISA, "IN", Country.GBR,
+//            LocalDate.now()
+//            );
 
     }
 }
