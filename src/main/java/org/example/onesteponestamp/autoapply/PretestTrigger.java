0bd21cace9710611fd155f89602ce7f4d45790ba
dabeen-jung <ltea99@naver.com>
2024-07-25 10:56:30 +0900

    chore : DB연결 세팅

---
+++ src/main/java/org/example/onesteponestamp/autoapply/PretestTrigger.java
@@ -0,0 +1,44 @@
+package org.example.onesteponestamp.autoapply;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PretestTrigger {
+
+  public static void main(String[] args) {
+    Connection conn = null;
+
+    try {
+      //1. 오라클 JDBC DRIVER를 메모리로 로딩
+      Class.forName("oracle.jdbc.OracleDriver");
+
+      //2.DB 연결
+      conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/xe", "java", "oracle");
+      System.out.println("연결 성공");
+
+      //3. DB 작업
+
+
+
+
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      if(conn != null) {
+        try {
+          conn.close();//열었던 자원 해제
+        } catch(SQLException e) {
+
+        }
+        //4. DB 연결 해제
+        System.out.println("연결 끊기");
+      }
+
+    }
+  }
+}
+
