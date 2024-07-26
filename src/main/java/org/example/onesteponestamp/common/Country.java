ee198a32f46b92cac72add58128890fce4f5125c
HJunng <guswjd1gh@gmail.com>
2024-07-26 13:57:35 +0900

    feat : 국가명, 비자명으로 enum 타입 찾아오는 함수 추가

--- src/main/java/org/example/onesteponestamp/common/Country.java
+++ src/main/java/org/example/onesteponestamp/common/Country.java
@@ -1,22 +1,45 @@
 package org.example.onesteponestamp.common;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum Country {
-  USA("USA", "United States", 0),
-  GBR("GBR", "United Kingdom", 0),
-  JPN("JPN", "Japan", 0),
-  FRA("FRA", "France", 0),
-  VNM("VNM", "Vietnam", 0),
-  KOR("KOR", "South Korea", 0),
-  PRK("PRK", "North Korea", 1),
-  IRQ("IRQ", "Iraq", 1),
-  RUS("RUS", "Russia", 1);
+  USA("USA", "United States", "SAFETY"),
+  GBR("GBR", "United Kingdom", "SAFETY"),
+  JPN("JPN", "Japan", "SAFETY"),
+  FRA("FRA", "France", "SAFETY"),
+  VNM("VNM", "Vietnam", "SAFETY"),
+  KOR("KOR", "South Korea", "SAFETY"),
+  PRK("PRK", "North Korea", "DANGER"),
+  IRQ("IRQ", "Iraq", "DANGER"),
+  RUS("RUS", "Russia", "DANGER");
 
   private final String countryCode;
   private final String countryName;
-  private final int dangerLevel;
+  private final String isDanger;
+
+  private static final Map<String, Country> NAME_TO_COUNTRY_MAP = new HashMap<>();
+
+  static {
+    for (Country c : Country.values()) {
+      NAME_TO_COUNTRY_MAP.put(c.getCountryName(), c);
+    }
+  }
+
+  /**
+   * 나라 이름으로 Country enum 타입 찾기
+   *
+   * @param countryName 나라 이름(영어 풀네임)
+   * @return Country(Enum)
+   */
+  public static Country getCountry(String countryName) {
+    if (!NAME_TO_COUNTRY_MAP.containsKey(countryName)) {
+      throw new IllegalArgumentException("Country code " + countryName + " not found");
+    }
+    return NAME_TO_COUNTRY_MAP.get(countryName);
+  }
 }
