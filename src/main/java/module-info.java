43a8e567cb316cfc5019e15f7607750ee6ba9fa7
HJunng <guswjd1gh@gmail.com>
2024-07-26 19:03:32 +0900

    chore : JavaFX 모듈 의존성 추가

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,19 @@
 module org.example.onesteponestamp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.web;
+  requires javafx.base; // JavaFX Base 모듈을 사용하기 위해 필요 (기본 JavaFX 기능 포함)
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires java.sql;
-    requires static lombok;
+  requires org.controlsfx.controls;
+  requires com.dlsc.formsfx;
+  requires net.synedra.validatorfx;
+  requires org.kordamp.ikonli.javafx;
+  requires org.kordamp.bootstrapfx.core;
+  requires eu.hansolo.tilesfx;
+  requires java.sql;
+  requires static lombok;
 
-    opens org.example.onesteponestamp to javafx.fxml;
-    exports org.example.onesteponestamp;
+  opens org.example.onesteponestamp to javafx.fxml;
+  opens org.example.onesteponestamp.autoapply to javafx.base; // JavaFX Base 모듈이 org.example.onesteponestamp.autoapply 패키지에 있는 클래스를 리플렉션을 통해 접근할 수 있도록 허용
+  exports org.example.onesteponestamp;
 }
