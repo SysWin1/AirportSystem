d31ca9c2ec3c9f97d430bde44df6e2fc936c84ef
ssoulistic <ssoulistic@gmail.com>
2024-07-29 20:02:49 +0900

    refactor : 중복 exception 제거

--- src/main/java/org/example/onesteponestamp/foreigner/ForeignerDAO.java
+++ src/main/java/org/example/onesteponestamp/foreigner/ForeignerDAO.java
@@ -16,7 +16,7 @@ public class ForeignerDAO {
 
   // 1. 외국인 목록 조회
   // Get all Foreigner records
-  public static List<ForeignerDTO> getAllForeigners() throws SQLException {
+  public static List<ForeignerDTO> getAllForeigners() {
     String sql = "select * from foreignvisitor where departure_status='STAY'";
     List<ForeignerDTO> foreigners = new ArrayList<>();
     try {
@@ -35,17 +35,15 @@ public class ForeignerDAO {
                 .build()
         );
       }
-
     } catch (SQLException e) {
       e.printStackTrace();
     }
-
     return foreigners;
   }
 
   // 2. 불법 체류 외국인 목록 조회.
   //  우선 connection을 받아서 조회 쿼리 실행 => return 값을 리스트 형식으로 받기.
-  public static List<ForeignerDTO> getIllegalForeigners() throws SQLException {
+  public static List<ForeignerDTO> getIllegalForeigners() {
     String sql = "select * from foreignvisitor where illegal_stay='ILLEGAL'";
     List<ForeignerDTO> foreigners = new ArrayList<>();
     try {
@@ -69,5 +67,4 @@ public class ForeignerDAO {
     }
     return foreigners;
   }
-
 }
