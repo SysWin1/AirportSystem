30da86e84cf99211e8c57f1528df05c4aae325a1
HJunng <guswjd1gh@gmail.com>
2024-07-29 14:08:49 +0900

    refactor : 주석 삭제 및 이전 코드 파일 리팩터링

--- src/main/java/org/example/onesteponestamp/HelloApplication.java
+++ src/main/java/org/example/onesteponestamp/HelloApplication.java
@@ -1,10 +1,10 @@
 package org.example.onesteponestamp;
 
-import java.io.IOException;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import org.example.onesteponestamp.admin.AuthManager;
 import org.example.onesteponestamp.immigration.ImmigrationDAO;
 import org.example.onesteponestamp.javafx.SelectionMain;
 
@@ -13,20 +13,16 @@ public class HelloApplication extends Application {
 
   private BorderPane mainLayout;
   private static final ImmigrationDAO immigrationDAO = new ImmigrationDAO();
-
+  private AuthManager authManager;
 
   @Override
-  public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        stage.setTitle("Hello!");
-//        stage.setScene(scene);
-//        stage.show();
+  public void start(Stage stage) {
 
     stage.setTitle("One Step - One Stamp");
 
     mainLayout = new BorderPane();
     SelectionMain selectionMain = new SelectionMain(mainLayout);
+    authManager = new AuthManager();
 
     Scene scene = new Scene(mainLayout, 800, 600);
     stage.setScene(scene);
